ad97e44c32b1d0dbc40922871d2b11a7af678c2d
Humaid Shaykh <122359199+humaidshaykh@users.noreply.github.com>
2023-07-28 07:20:35 +0500

    qamber-corporate-services

---
+++ images/main.js
@@ -0,0 +1,132 @@
+a1 = document.getElementById("a1");
+
+a1.addEventListener('click', function () {
+    document.getElementById("div1").style.display = 'block';
+    document.getElementById("div2").style.display = 'none';
+    document.getElementById("div3").style.display = 'none';
+    document.getElementById("div4").style.display = 'none';
+    document.getElementById("div5").style.display = 'none';
+    document.getElementById("div6").style.display = 'none';
+    document.getElementById("div7").style.display = 'none';
+    document.getElementById("z1").style.backgroundColor = "#F5F5F5";
+    document.getElementById("z2").style.backgroundColor = "transparent";
+    document.getElementById("z3").style.backgroundColor = "transparent";
+    document.getElementById("z4").style.backgroundColor = "transparent";
+    document.getElementById("z5").style.backgroundColor = "transparent";
+    document.getElementById("z6").style.backgroundColor = "transparent";
+    document.getElementById("z7").style.backgroundColor = "transparent";
+});
+
+a2 = document.getElementById("a2");
+
+a2.addEventListener('click', function () {
+    document.getElementById("div1").style.display = 'none';
+    document.getElementById("div2").style.display = 'block';
+    document.getElementById("div3").style.display = 'none';
+    document.getElementById("div4").style.display = 'none';
+    document.getElementById("div5").style.display = 'none';
+    document.getElementById("div6").style.display = 'none';
+    document.getElementById("div7").style.display = 'none';
+    document.getElementById("z1").style.backgroundColor = "transparent";
+    document.getElementById("z2").style.backgroundColor = "#F5F5F5";
+    document.getElementById("z3").style.backgroundColor = "transparent";
+    document.getElementById("z4").style.backgroundColor = "transparent";
+    document.getElementById("z5").style.backgroundColor = "transparent";
+    document.getElementById("z6").style.backgroundColor = "transparent";
+    document.getElementById("z7").style.backgroundColor = "transparent";
+});
+
+a3 = document.getElementById("a3");
+
+a3.addEventListener('click', function () {
+    document.getElementById("div1").style.display = 'none';
+    document.getElementById("div2").style.display = 'none';
+    document.getElementById("div3").style.display = 'block';
+    document.getElementById("div4").style.display = 'none';
+    document.getElementById("div5").style.display = 'none';
+    document.getElementById("div6").style.display = 'none';
+    document.getElementById("div7").style.display = 'none';
+    document.getElementById("z1").style.backgroundColor = "transparent";
+    document.getElementById("z2").style.backgroundColor = "transparent";
+    document.getElementById("z3").style.backgroundColor = "#F5F5F5";
+    document.getElementById("z4").style.backgroundColor = "transparent";
+    document.getElementById("z5").style.backgroundColor = "transparent";
+    document.getElementById("z6").style.backgroundColor = "transparent";
+    document.getElementById("z7").style.backgroundColor = "transparent";
+});
+
+a4 = document.getElementById("a4");
+
+a4.addEventListener('click', function () {
+    document.getElementById("div1").style.display = 'none';
+    document.getElementById("div2").style.display = 'none';
+    document.getElementById("div3").style.display = 'none';
+    document.getElementById("div4").style.display = 'block';
+    document.getElementById("div5").style.display = 'none';
+    document.getElementById("div6").style.display = 'none';
+    document.getElementById("div7").style.display = 'none';
+    document.getElementById("z1").style.backgroundColor = "transparent";
+    document.getElementById("z2").style.backgroundColor = "transparent";
+    document.getElementById("z3").style.backgroundColor = "transparent";
+    document.getElementById("z4").style.backgroundColor = "#F5F5F5";
+    document.getElementById("z5").style.backgroundColor = "transparent";
+    document.getElementById("z6").style.backgroundColor = "transparent";
+    document.getElementById("z7").style.backgroundColor = "transparent";
+});
+
+a5 = document.getElementById("a5");
+
+a5.addEventListener('click', function () {
+    document.getElementById("div1").style.display = 'none';
+    document.getElementById("div2").style.display = 'none';
+    document.getElementById("div3").style.display = 'none';
+    document.getElementById("div4").style.display = 'none';
+    document.getElementById("div5").style.display = 'block';
+    document.getElementById("div6").style.display = 'none';
+    document.getElementById("div7").style.display = 'none';
+    document.getElementById("z1").style.backgroundColor = "transparent";
+    document.getElementById("z2").style.backgroundColor = "transparent";
+    document.getElementById("z3").style.backgroundColor = "transparent";
+    document.getElementById("z4").style.backgroundColor = "transparent";
+    document.getElementById("z5").style.backgroundColor = "#F5F5F5";
+    document.getElementById("z6").style.backgroundColor = "transparent";
+    document.getElementById("z7").style.backgroundColor = "transparent";
+});
+
+a6 = document.getElementById("a6");
+
+a6.addEventListener('click', function () {
+    document.getElementById("div1").style.display = 'none';
+    document.getElementById("div2").style.display = 'none';
+    document.getElementById("div3").style.display = 'none';
+    document.getElementById("div4").style.display = 'none';
+    document.getElementById("div5").style.display = 'none';
+    document.getElementById("div6").style.display = 'block';
+    document.getElementById("div7").style.display = 'none';
+    document.getElementById("z1").style.backgroundColor = "transparent";
+    document.getElementById("z2").style.backgroundColor = "transparent";
+    document.getElementById("z3").style.backgroundColor = "transparent";
+    document.getElementById("z4").style.backgroundColor = "transparent";
+    document.getElementById("z5").style.backgroundColor = "transparent";
+    document.getElementById("z6").style.backgroundColor = "#F5F5F5";
+    document.getElementById("z7").style.backgroundColor = "transparent";
+});
+
+a7 = document.getElementById("a7");
+
+a7.addEventListener('click', function () {
+    document.getElementById("div1").style.display = 'none';
+    document.getElementById("div2").style.display = 'none';
+    document.getElementById("div3").style.display = 'none';
+    document.getElementById("div4").style.display = 'none';
+    document.getElementById("div5").style.display = 'none';
+    document.getElementById("div6").style.display = 'none';
+    document.getElementById("div7").style.display = 'block';
+    document.getElementById("z1").style.backgroundColor = "transparent";
+    document.getElementById("z2").style.backgroundColor = "transparent";
+    document.getElementById("z3").style.backgroundColor = "transparent";
+    document.getElementById("z4").style.backgroundColor = "transparent";
+    document.getElementById("z5").style.backgroundColor = "transparent";
+    document.getElementById("z6").style.backgroundColor = "transparent";
+    document.getElementById("z7").style.backgroundColor = "#F5F5F5";
+});
